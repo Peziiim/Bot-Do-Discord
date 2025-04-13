7a821abbd97a43d9edf8bcd1f0162b919ad76de5
Peziiim <eduardoorx@gmail.com>
2025-04-12 23:45:36 -0300

    feat: added Discord Bot

--- botdiscord/src/main/java/com/example/Main.java
+++ botdiscord/src/main/java/com/example/Main.java
@@ -3,6 +3,7 @@ package com.example;
 import java.util.EnumSet;
 
 import com.example.commands.Ping;
+import com.example.commands.TokenBot;
 
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
@@ -11,9 +12,10 @@ import net.dv8tion.jda.api.requests.GatewayIntent;
 public class Main {
     
     public static JDA jda;
+    private static TokenBot tok;
     public static void main(String[] args) {
 
-            jda = JDABuilder.create("MTM2MDc0NjY5NDI4Njc3NDMzNA.GgIeGZ.Ytosa6A-84WcWpNMcfKn3cOQpWA27Q9TWZEFG8",
+            jda = JDABuilder.create(tok.getToken(),
                 EnumSet.allOf(GatewayIntent.class)).build();
 
             jda.addEventListener(new Ping());
---
+++ botdiscord/src/main/java/com/example/commands/TokenBot.java
@@ -0,0 +1,15 @@
+package com.example.commands;
+
+public class TokenBot {
+    private String token = "MTM2MDc0NjY5NDI4Njc3NDMzNA.GgIeGZ.Ytosa6A-84WcWpNMcfKn3cOQpWA27Q9TWZEFG8";
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    
+}
